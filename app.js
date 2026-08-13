09c92e1cb2ab678e1f8ced1231497a0607922c3e
pickdelcomic <micahdelangel1909@gmail.com>
2026-08-13 11:45:39 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
-console.log("Visual Notes is running!");
+const notes = document.querySelectorAll(".note");
+
+notes.forEach(note => {
+    let dragging = false;
+    let offsetX = 0;
+    let offsetY = 0;
+
+    note.addEventListener("mousedown", function (event) {
+        dragging = true;
+
+        offsetX = event.clientX - note.offsetLeft;
+        offsetY = event.clientY - note.offsetTop;
+
+        note.style.zIndex = 100;
+    });
+
+    document.addEventListener("mousemove", function (event) {
+        if (!dragging) return;
+
+        note.style.left = (event.clientX - offsetX) + "px";
+        note.style.top = (event.clientY - offsetY) + "px";
+    });
+
+    document.addEventListener("mouseup", function () {
+        dragging = false;
+    });
+});
